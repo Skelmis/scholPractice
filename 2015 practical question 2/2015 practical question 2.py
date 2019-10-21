acadc39c264d88b047850312b845f453125c81b7
Skelmis <Skelmis.craft@gmail.com>
2019-10-22 11:18:50 +1300

    19 minutes in. Ideally this question should be the one out of the three that takes less time. Overall exam is 6 hours. Recommend around an hour for this as it is always the easiest task from what I have seen

--- 2015 practical question 2/2015 practical question 2.py
+++ 2015 practical question 2/2015 practical question 2.py
@@ -1,5 +1,12 @@
-inputOne = "10:9.8"#placeholder values
-inputTwo = "5:4.3"
+#Goal:
+#To create a program that takes two inputs and outputs the time difference
+
+#Ideas:
+#Take input as a string, split it based on special characters into an array
+#Work on each item individually to calculate differences
+
+inputOne = "1:10.20"#placeholder values
+inputTwo = "5:20.10"
 
 def SplitInput(input):#splits the input into the relevant format hour - minute - seconds
     splitInputOne = input.split(":")
@@ -8,4 +15,22 @@ def SplitInput(input):#splits the input into the relevant format hour - minute -
     finishedFirstInput = [f'{splitInputOne[0]}',f'{splitInputOneCarry[0]}',f'{splitInputOneCarry[1]}']
     return finishedFirstInput
 
-print(SplitInput(inputTwo))
+def GetDifference(first, second):#Takes two ints, finds the difference between the second and first
+    first = int(first)
+    second = int(second)
+    difference = second - first
+    return difference
+
+
+#Getting users input split into the relevant parts
+one = SplitInput(inputOne)
+two = SplitInput(inputTwo)
+
+secondDifference = GetDifference(one[2], two[2])
+print(secondDifference)
+
+mintueDifference = GetDifference(one[1], two[1])
+print(mintueDifference)
+
+hourDifference = GetDifference(one[0], two[0])
+print(hourDifference)
